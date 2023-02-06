65931d7297446a3b973c8c28a568b354ee2a8124
GostGas6 <you@example.com>
2023-02-06 22:26:55 +0300

    поправить ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
-const openEdit = document.querySelector('.profile__info-edit-button');
+const openEdit = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const closeEdit = document.querySelector('.popup__container-close-icon');
-const nameInput = document.querySelector('.popup__container-name');
-const jobInput = document.querySelector('.popup__container-text');
-const nameValue = document.querySelector('.profile__info-title');
-const jobValue = document.querySelector('.profile__info-subtitle');
+const closeEdit = document.querySelector('.popup__close-icon');
+const nameInput = document.querySelector('.popup__name');
+const jobInput = document.querySelector('.popup__text');
+const nameValue = document.querySelector('.profile__title');
+const jobValue = document.querySelector('.profile__subtitle');
 
 function openEditClick() {
     popup.classList.add('popup_opened');
@@ -20,7 +20,7 @@ function transferText() {
     openEditClick();
 }
 
-const formElement = document.querySelector('.popup__container-button-save');
+const formElement = document.querySelector('.popup__button-save');
 
 function handleFormSubmit(event) {
     event.preventDefault();
