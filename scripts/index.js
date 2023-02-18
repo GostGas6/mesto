747104fc5b07ee7f069a8ab823a9761c8737aece
GostGas6 <you@example.com>
2023-02-18 22:30:14 +0300

    создал попап добавления

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,12 @@ const aboutInput = document.querySelector('#popup_about')
 const nameValue = document.querySelector('.profile__title');
 const jobValue = document.querySelector('.profile__subtitle');
 const formElement = document.querySelector('.userData');
+const popupForm = document.querySelector('.popup-form');
+const openAdd = document.querySelector('.profile__add-button');
+const closeEditForm = document.querySelector('.popup-form__close-button');
+const nameInputForm = document.querySelector('#popup_place');
+const placeInputForm = document.querySelector('#popup_link');
+
 
 function closeEditClick() {
     popup.classList.remove('popup_opened');
@@ -24,6 +30,17 @@ function handleFormSubmit(event) {
     closeEditClick();
 }
 
+function openAddButton () {
+    popupForm.classList.add('popup-form_opened');
+}
+
+function closeAddButton () {
+    popupForm.classList.remove('popup-form_opened');
+}
+
+
+openAdd.addEventListener('click', openAddButton);
+closeEditForm.addEventListener('click', closeAddButton);
 openEdit.addEventListener('click', transferText);
 closeEdit.addEventListener('click', closeEditClick);
 formElement.addEventListener('submit', handleFormSubmit);
